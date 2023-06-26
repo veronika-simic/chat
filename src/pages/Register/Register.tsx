f91dfd497d0c2ca9596da0ecb7c6fc6f275d8608
veronika-simic <veronikasimic21@gmail.com>
2023-06-26 14:11:42 +0200

    change color, add events

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,10 +1,27 @@
+import { useState } from "react";
 const Register = () => {
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
   return (
-    <div className="bg-teal-300 h-screen">
-      <form className="w-64 mx-auto">
-        <input type="text" placeholder="username" className="block w-full rounded-sm p-2 mb-2 border"></input>
-        <input type="password" placeholder="password" className="block w-full rounded-sm p-2 mb-2 border"></input>
-        <button className="bg-slate-50 block w-full rounded-sm p-2">Register</button>
+    <div className="bg-slate-100 h-screen flex items-center">
+      <form className="w-64 mx-auto mb-12">
+        <input
+          value={username}
+          type="text"
+          placeholder="username"
+          className="block w-full rounded-sm p-2 mb-2 border"
+          onChange={(e) => setUserName(e.target.value)}
+        ></input>
+        <input
+          value={password}
+          type="password"
+          placeholder="password"
+          className="block w-full rounded-sm p-2 mb-2 border"
+          onChange={(e) => setPassword(e.target.value)}
+        ></input>
+        <button className="bg-sky-600 block w-full rounded-sm p-2 text-white">
+          Register
+        </button>
       </form>
     </div>
   );
