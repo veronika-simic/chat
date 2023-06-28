3812ef4c64865bc3e30463051b06ced62344b268
veronika-simic <veronikasimic21@gmail.com>
2023-06-28 06:24:31 +0200

    infer types for user context

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -1,16 +1,26 @@
 import { ReactNode, createContext, useState } from "react";
 
 interface Props {
-    children?: ReactNode
+  children?: ReactNode;
 }
 
-export const UserContext = createContext({});
+export const UserContext = createContext<{
+  userName: string;
+  setUserName: React.Dispatch<React.SetStateAction<string>>;
+  id: string;
+  setId: React.Dispatch<React.SetStateAction<string>>;
+}>({
+  userName: '',
+  setUserName: () => undefined,
+  id: '',
+  setId: () => undefined,
+});
 
-export function UserContextProvider({ children}: Props ) {
-  const [userName, setUserName] = useState(null);
-  const [id, setId] = useState(null);
+export function UserContextProvider({ children }: Props) {
+  const [userName, setUserName] = useState<string>('');
+  const [id, setId] = useState<string>('');
   return (
-    <UserContext.Provider value={{userName, setUserName, id, setId}}>
+    <UserContext.Provider value={{ userName, setUserName, id, setId }}>
       {children}
     </UserContext.Provider>
   );
--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,14 +1,17 @@
-import { useState } from "react";
-import axios from 'axios';
+import { useContext, useState } from "react";
+import axios from "axios";
+import { UserContext } from "../../context/UserContext";
 
 const Register = () => {
-  const [username, setUserName] = useState("");
+  const [username, setUserName] = useState<string>("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const { setUserName: setLoggedInUserName, setId } = useContext(UserContext);
   async function register(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault()
-    await axios.post('/register', {username, password})
-
+    e.preventDefault();
+    const { data } = await axios.post("/register", { username, password });
+    setLoggedInUserName(username);
+    setId(data.id);
   }
   return (
     <div className="bg-slate-100 h-screen flex items-center">
